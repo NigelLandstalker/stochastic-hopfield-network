f4ab4a65714460d1449a34e76c254a110cc00f4b
Owen Hoffend <mcnewomaster@gmail.com>
2017-04-12 09:52:49 -0500

    Added input name similarity detection and memory element simulation within bitstream_simulation

--- bitstream_simulation.py
+++ bitstream_simulation.py
@@ -7,6 +7,8 @@ import sys
 
 #Functions in this simulator use the True/False functionality of stochasticUtils
 
+MAX_RUNS = 1000 #Maximum iterations of a top-level circuit
+
 def main(argv): #For command-line operation (implement later)
 	print(str(argv))
 
@@ -15,21 +17,60 @@ class Circuit():
 		self.left = left
 		self.right = right
 		self.inputs = []
+		self.input_names = []
 
-		if isinstance(left, Input):
-			self.inputs.append(left)
-		elif isinstance(left, Circuit):
-			self.inputs += left.inputs
+		self.run_count = 0
 
-		if isinstance(right, Input):
-			self.inputs.append(right)
-		elif isinstance(right, Circuit):
-			self.inputs += right.inputs
+		def __add_input(new_input): #In the future this could work for circuits with > 2 inputs.
+			if isinstance(new_input, Input):
+				self.inputs.append(new_input)
+				self.input_names.append(new_input.name)
+			elif isinstance(new_input, Circuit):
+				self.inputs += new_input.inputs
+				self.input_names += [_input.name for _input in new_input.inputs]
+
+		__add_input(left)
+		__add_input(right)
 
 		self.num_inputs = len(self.inputs)
 
+#	def replace_input(self, name_to_replace, new_input): #Used for building circuits with feedback
+#		index = self.input_names.index(name_to_replace)
+#		self.input_names[index] = name_to_replace
+#		self.inputs[index] = new_input
+#
+#		if name_to_replace in self.left.input_names:
+#			self.left = new_input
+#		else:
+#			self.right = new_input
+
+	def set_input(self, name, value):
+		if name in self.input_names:
+			for index, current_name in enumerate(self.input_names):
+				if current_name == name:
+					self.inputs[index].value = value
+		else:
+			raise ValueError("No variable of name %s exists" % name)
+
 	def get_inputs(self):
 		return [_input.name + " " + str(_input.value) for _input in self.inputs]
+
+class Rsnor(Circuit): #Internal memory element instead of dealing with feedback within the recursion tree.
+	def __init__(self, left, right):
+		Circuit.__init__(self, left, right) #left = S, right = R
+		self.memory = False
+
+	def run(self):
+		s = self.left.run()
+		r = self.right.run()
+		if s and r:
+			raise ValueError("RS Nor is in an invalid state")
+		elif s and (not r):
+			self.memory = True
+		elif (not s) and r:
+			self.memory = False
+
+		return self.memory
 
 class And(Circuit):
 	def __init__(self, left, right):
@@ -65,17 +106,22 @@ if __name__ == "__main__":
 		#Do the default thing
 		circuit = And(Or(Input('A', False), Input('B')), Input('C', True))
 		xor = Or(And(Input('X'), Not(Input('Y'))), And(Not(Input('X')), Input('Y')))
+		xor.set_input('X', True)
+		xor.set_input('Y', True)
 
-		_input = xor.inputs
-
-		_input[0].value = False
-		_input[1].value = False
-		_input[2].value = False
-		_input[3].value = False
+		rsnor = Rsnor(Input('S'), Input('R'))
+		rsnor.set_input('S', True)
+		print(rsnor.run())
+		rsnor.set_input('S', False)
+		print(rsnor.run())
+		rsnor.set_input('R', True)
+		print(rsnor.run())
+		rsnor.set_input('R', False)
+		print(rsnor.run())
 
 		_input_str = xor.get_inputs()
 		print(_input_str)
 		print(xor.run())
-		#su.gen_prob()
+
 	else:
 		main(sys.argv[1:]) #TODO: Implement parsing of equatons later
